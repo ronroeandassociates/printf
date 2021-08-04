dfea422651fd1113c774a6adec1b3a8d56a49697
ronroeandassociates <3321@holbertonschool.com>
2021-08-03 20:00:23 -0700

    rewrite holberton and _printf

--- _printf.c
+++ _printf.c
@@ -1,84 +1,53 @@
-#include <unistd.h>
 #include "holberton.h"
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * buffer_print - print given buffer to stdout
- * @buffer: buffer to print
- * @nbytes: number of bytes to print
+ * _printf - prints a string with functionality
+ * @format: the string to print
  *
- * Return: nbytes
-  */
-int buffer_print(char buffer[], unsigned int nbytes)
-{
-	write(1, buffer, nbytes);
-	return (nbytes);
-}
-
-/**
- * buffer_add - adds a string to buffer
- * @buffer: buffer to fill
- * @str: str to add
- * @buffer_pos: pointer to buffer first empty position
- *
- * Return: if buffer filled and emptyed return number of printed char
- * else 0
+ * Return: 0 Always success
  */
-int buffer_add(char buffer[], char *str, unsigned int *buffer_pos)
-{
-	int i = 0;
-	unsigned int count = 0, pos = *buffer_pos, size = BUFFER_SIZE;
 
-	while (str && str[i])
-	{
-		if (pos == size)
-		{
-			count += buffer_print(buffer, pos);
-			pos = 0;
-		}
-		buffer[pos++] = str[i++];
-	}
-	*buffer_pos = pos;
-	return (count);
-}
-
-/**
- * _printf - produces output according to a format
- * @format: character string
- *
- * Return: the number of characters printed excluding the null byte
- * used to end output to strings
- */
 int _printf(const char *format, ...)
 {
+	int i, j, counter = 0;
 	va_list ap;
-	unsigned int i = 0, buffer_pos = 0, count = 0;
-	char *res_str, *aux, buffer[BUFFER_SIZE];
-
-	if (!format || !format[0])
+	pt_t types[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"d", print_d},
+		{"%", print_p},
+		{"i", print_d},
+		{NULL, NULL},
+	};
+	if (format == NULL)
 		return (-1);
-		va_start(ap, format);
-		aux = malloc(sizeof(char) * 2);
-		while (format && format[i])
+	va_start(ap, format);
+	i = 0;
+	while (format != NULL && format[i] != '\0')
+	{
+	if (format[i] == '%')
+	{
+		i++;
+		if (format[i] == '\0')
 		{
-			if (format[i] == '%')
-			{
-				res_str = treat_format(format, &i, ap);
-				count += buffer_add(buffer, res_str, &buffer_pos);
-				free(res_str);
-			}
-			else
-			{
-				aux[0] = format[i++];
-				aux[1] = '\0';
-				count += buffer_add(buffer, aux, &buffer_pos);
-			}
+			return (-1);
 		}
-		count += buffer_print(buffer, buffer_pos);
-		free(aux);
-		va_end(ap);
-		if (!count)
-			count = -1;
-		return (count);
+		j = 0;
+		while (types[j].fs != NULL)
+		{
+			if (*(types[j].fs) == format[i])
+				counter += types[j].f(ap);
+			j++;
+		}
+				i++;
+		}
+		if (format[i] != '%' && format[i] != '\0')
+		{
+			counter += _putchar(format[i]);
+			i++;
+		}
+	}
+	va_end(ap);
+	return (counter);
 }
-
--- holberton.h
+++ holberton.h
@@ -1,60 +1,31 @@
-#ifndef _FUNCTION_PRINTF_H_
-#define _FUNCTION_PRINTF_H_
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+#include <unistd.h>
 #include <stdarg.h>
 #include <stdlib.h>
-#include <stddef.h>
 
-#define BUFFER_SIZE 1024
+void _puts(char *str);
+int _putchar(char c);
+int _printf(const char *format, ...);
+int _strlen(const char *s);
 
 /**
- * struct modifier - mofifier fields collection
- * @flags: flags field composed of ['0', ' ', '#', '+', '-']
- * @width: width field, positive number
- * @precision: precision field positive number not including '.'
- * or -1 for '*'
- * @length: length field string composed of ['h', 'l']
- * @specifier: specifier character can one of
- * ['c', 's', '%', 'd', 'i', 'b', 'u', 'o', 'x', 'X', 'S', 'p', 'r', 'R']
- *
- */
-typedef struct modifier
+ *  * struct print_type - structure for different types of cases
+ *   * @fs: function specifiers
+ *    * @f: function pointer
+ *     */
+
+typedef struct print_type
 {
-	char *flags;
-	int width;
-	int precision;
-	char *length;
-	char specifier;
-} modifier_t;
+		char *fs;
+			int (*f)(va_list);
+} pt_t;
 
-void *_realloc(void *ptr, unsigned int, unsigned int);
-int _putchar(char c);
-char *print_binary(modifier_t *, va_list);
-char *print_unsigned_int(modifier_t *, va_list);
-char *print_octal(modifier_t *, va_list);
-char *print_hex(modifier_t *, va_list);
-char *print_char(modifier_t *, va_list ap);
-char *print_int(modifier_t *, va_list ap);
-char *print_string(modifier_t *modif, va_list ap);
-char *print_rev(modifier_t *, va_list ap);
-char *print_big_s(modifier_t *, va_list);
-char *print_pointer(modifier_t *, va_list);
-char *rot13(char *s);
-int _strlen(char *s);
-char *print_rot(modifier_t *, va_list ap);
+int print_c(va_list ap);
+int print_s(va_list ap);
+int print_d(va_list ap);
+int print_p(va_list ap);
+int printnumber(int n);
 
-void free_modifier(modifier_t *);
-char *get_flags(const char *, unsigned int *);
-int get_width(const char *, unsigned int *);
-int get_precision(const char *, unsigned int *);
-char *get_length(const char *, unsigned int *);
-char get_specifier(const char *, unsigned int *);
-modifier_t *get_modifier(const char *, unsigned int *);
-char *treat_format(const char *, unsigned int *, va_list);
-int _printf(const char *format, ...);
-char *_strcpy(char *dest, char *src);
-void array_rev(char *arr, int len);
-int int_len(int num);
-char *ito(int n);
-char *reverse(char *s);
-
-#endif 
+#endif
